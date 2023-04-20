4f227fa18992e67c2845bfe8ec94b119d223d766
Sasha <99426675+sashkathewizard@users.noreply.github.com>
2023-04-20 18:11:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.project-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+  
